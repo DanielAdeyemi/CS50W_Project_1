c84d0961e9da41e790d2c30627f68dafa0ff9e1e
DanielAdeyemi <adeyemidany@gmail.com>
2022-05-31 19:19:18 -0700

    The view get the content of the encyclopedia entry by calling the appropriate util function.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def topics(request, topic):
+    index = 0
+    topicsIn = util.list_entries()
+    for i, topicIn in enumerate(topicsIn):
+        if(topic == topicIn.lower()):
+            index = i
+    return render(request, "encyclopedia/topic.html", {
+        "topic": util.get_entry(topic)
+    })
